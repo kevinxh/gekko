e84dd8b384f0b67792344bec83d4ed2b015d329d
Kevin He <k.he933@gmail.com>
2018-02-15 23:34:26 -0800

    RSI custom strategy

--- strategies/custom.js
+++ strategies/custom.js
@@ -1,63 +1,125 @@
-// This is a basic example strategy for Gekko.
-// For more information on everything please refer
-// to this document:
-//
-// https://gekko.wizb.it/docs/strategies/creating_a_strategy.html
-//
-// The example below is pretty bad investment advice: on every new candle there is
-// a 10% chance it will recommend to change your position (to either
-// long or short).
+/*
 
-var log = require('../core/log');
+  RSI - cykedev 14/02/2014
 
-// Let's create our own strat
-var strat = {};
+  (updated a couple of times since, check git history)
 
-// Prepare everything our method needs
-strat.init = function() {
-  this.input = 'candle';
-  this.currentTrend = 'long';
-  this.requiredHistory = 0;
+ */
+// helpers
+var _ = require('lodash');
+var log = require('../core/log.js');
+
+var RSI = require('./indicators/RSI.js');
+
+// let's create our own method
+var method = {};
+
+// prepare everything our method needs
+method.init = function() {
+  this.name = 'RSI';
+  this.inTrade = false;
+  this.inTradePrice = 0;
+  this.trend = {
+    direction: 'none',
+    duration: 0,
+    persisted: false,
+    adviced: false
+  };
+
+  this.requiredHistory = this.tradingAdvisor.historySize;
+
+  // define the indicators we need
+  this.addIndicator('rsi', 'RSI', this.settings);
 }
 
-// What happens on every new candle?
-strat.update = function(candle) {
-
-  // Get a random number between 0 and 1.
-  this.randomNumber = Math.random();
-
-  // There is a 10% chance it is smaller than 0.1
-  this.toUpdate = this.randomNumber < 0.1;
-}
-
-// For debugging purposes.
-strat.log = function() {
-  log.debug('calculated random number:');
-  log.debug('\t', this.randomNumber.toFixed(3));
-}
-
-// Based on the newly calculated
-// information, check if we should
-// update or not.
-strat.check = function() {
-
-  // Only continue if we have a new update.
-  if(!this.toUpdate)
-    return;
-
-  if(this.currentTrend === 'long') {
-
-    // If it was long, set it to short
-    this.currentTrend = 'short';
-    this.advice('short');
-
-  } else {
-
-    // If it was short, set it to long
-    this.currentTrend = 'long';
-    this.advice('long');
-
+method.update = function(candle) {
+  if(this.inTrade && this.inTradePrice*0.99 < candle.close.toFixed(2)) {
+      this.trend.adviced = false;
+      this.inTrade = false;
+      this.advice('short');
+      log.debug('sell');  
+      log.debug('\t', 'Stop loss triggered at price:', candle.close.toFixed(2));
   }
 }
 
-module.exports = strat;
+// for debugging purposes log the last
+// calculated parameters.
+method.log = function(candle) {
+  var digits = 8;
+  var rsi = this.indicators.rsi;
+
+  // log.debug('calculated RSI properties for candle:');
+  // log.debug('\t', 'rsi:', rsi.result.toFixed(digits));
+  // log.debug('\t', 'price:', candle.close.toFixed(digits));
+}
+
+method.check = function(candle) {
+  var rsi = this.indicators.rsi;
+  var rsiVal = rsi.result;
+
+  if(rsiVal > this.settings.thresholds.high) {
+
+    // new trend detected
+    if(this.trend.direction !== 'high')
+      this.trend = {
+        duration: 0,
+        persisted: false,
+        direction: 'high',
+        adviced: false
+      };
+
+    this.trend.duration++;
+
+    // log.debug('In high since', this.trend.duration, 'candle(s)');
+
+    if(this.trend.duration >= this.settings.thresholds.persistence)
+      this.trend.persisted = true;
+
+    if(this.trend.persisted && !this.trend.adviced) {
+      this.trend.adviced = true;
+      this.inTrade = false;
+      this.advice('short');
+      log.debug('sell');
+      log.debug(this.inTrade);      
+      log.debug('\t', 'price:', candle.close.toFixed(2));
+    } else
+      this.advice();
+
+  } else if(rsiVal < this.settings.thresholds.low) {
+
+    // new trend detected
+    if(this.trend.direction !== 'low')
+      this.trend = {
+        duration: 0,
+        persisted: false,
+        direction: 'low',
+        adviced: false
+      };
+
+    this.trend.duration++;
+
+    log.debug('In low since', this.trend.duration, 'candle(s)');
+
+    if(this.trend.duration >= this.settings.thresholds.persistence)
+      this.trend.persisted = true;
+
+    if(this.trend.persisted && !this.trend.adviced) {
+      this.trend.adviced = true;
+      this.inTrade = true;
+      this.inTradePrice = candle.close.toFixed(2);
+      this.advice('long');
+      log.debug('buy');
+      log.debug(this.inTrade);
+      log.debug('\t', 'price:', candle.close.toFixed(2));
+    } else
+      this.advice();
+
+  } else {
+
+    // log.debug('In no trend');
+
+    this.advice();
+  }
+}
+
+module.exports = method;
